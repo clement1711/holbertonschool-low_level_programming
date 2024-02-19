baef5fb1ec35b0c9d2775d528f460b590d53603a
clement1711 <corbiere.clement@hotmail.fr>
2024-02-19 14:36:02 +0000

    Size is not grandeur, and territory does not make a nation

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,5 +10,5 @@ printf("Taille de init : %d octets\n", sizeof(int));
 printf("Taille de long int : %d octets\n", sizeof(long int));
 printf("Taille de long long init : %d octets\n", sizeof(long long init));
 printf("Taille de float : %d octets\n", sizeof(float));
-return 0;
+return (0);
 }
