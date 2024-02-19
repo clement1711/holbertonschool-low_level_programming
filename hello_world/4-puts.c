aecce3b23f2260891e7d35eb4bef3e7f722a6bda
clement1711 <corbiere.clement@hotmail.fr>
2024-02-19 13:34:47 +0000

    hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include "Programming is like building a multilingual puzzle<stdio.h>
+#include <stdio.h>
 /**
  * main - main block
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"/n);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
