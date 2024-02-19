aba0820a2a88b718bea1530f940059349cb30a3d
clement1711 <corbiere.clement@hotmail.fr>
2024-02-19 14:29:28 +0000

    Size is not grandeur, and territory does not make a nation

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-
-int main() {
-    printf("Taille de char : %ld octets\n", sizeof(char));
-    printf("Taille de init : %ld octets\n", sizeof(int));
-    printf("Taille de long int : %ld octets\n", sizeof(long int));
-    printf("Taille de long long init : %ld octets\n", sizeof(long long init));
-    printf("Taille de float : %ld octets\n", sizeof(float));
-    return 0;
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+printf("Taille de char : %ld octets\n", sizeof(char));
+printf("Taille de init : %ld octets\n", sizeof(int));
+printf("Taille de long int : %ld octets\n", sizeof(long int));
+printf("Taille de long long init : %ld octets\n", sizeof(long long init));
+printf("Taille de float : %ld octets\n", sizeof(float));
+return 0;
 }
