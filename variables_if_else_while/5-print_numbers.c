cd72152ddc53fe1cd09326cce6fe05c41ea4da7c
clement1711 <clement.corbiere@hotmail.fr>
2024-02-21 15:27:54 +0000

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for(c == 1: c < 10; c ++) 
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
