6d2083d0c104456b19988ad5b97b5168cafa2287
clement1711 <clement.corbiere@hotmail.fr>
2024-02-21 14:24:52 +0000

    alphabet in lowercase

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * play what I call the alphabet game
+ * Return: 0
+ */
+int main(void)
+{
+        char alpha;
+	for(alpha = 'a'; alpha <= 'z'; ++alpha) {
+		if(alpha != 'q' && alpha != 'e') {
+			putchar("%d", alpha);
+		}	
+        }
+        putchar('\n');
+        return (0);
+}
