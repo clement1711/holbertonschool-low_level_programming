489e3b99e54d8347674574f3e4496be794d0b71e
clement1711 <clement.corbiere@hotmail.fr>
2024-02-21 15:21:20 +0000

    prints the alphabet in lowercase

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,10 @@
 int main(void)
 {
 char alpha;
-for (alpha = 'a'; alpha <= 'z'; ++alpha) {
-if (alpha != 'q' && alpha != 'e') {
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+if (alpha != 'q' && alpha != 'e')
 putchar(alpha);
-}	
-}
 putchar('\n');
 return (0);
 }
