f43ee62c90f17b19ef4d2d437cbb3d1466666a07
clement1711 <clement.corbiere@hotmail.fr>
2024-02-21 13:42:21 +0000

    alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,17 @@
 int main() {
     char lowercase, uppercase;
 
-    printf("Alphabet en minuscules : \n");
+    putchar("Alphabet en minuscules : \n");
     for(lowercase = 'a'; lowercase <= 'z'; ++lowercase) {
         printf("%c ", lowercase);
     }
 
-    printf("\nAlphabet en majuscules : \n");
+    putchar("\nAlphabet en majuscules : \n");
     for(uppercase = 'A'; uppercase <= 'Z'; ++uppercase) {
         printf("%c ", uppercase);
     }
 
-    printf("\n");
+    putchar("\n");
 
     return 0;
 }
