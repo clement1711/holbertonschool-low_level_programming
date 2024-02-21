ae5989ad528a604b3812533c7914763875a4f4d9
clement1711 <clement.corbiere@hotmail.fr>
2024-02-21 14:36:24 +0000

    alphabet in lowercase

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 char alpha;
-for(alpha = 'a'; alpha <= 'z'; ++alpha){
-	if(alpha != 'q' && alpha != 'e'){
-	putchar(alpha);
+for (alpha = 'a'; alpha <= 'z'; ++alpha) {
+if (alpha != 'q' && alpha != 'e') {
+putchar(alpha);
 }	
 }
 putchar('\n');
