851deac1c781d2bec24d7763f9d70874731dea18
clement1711 <clement.corbiere@hotmail.fr>
2024-02-22 14:53:08 +0000

    add main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main -  programme main.h
+ *
+ * Return: Toujours 0 (Succès)
+ */
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
