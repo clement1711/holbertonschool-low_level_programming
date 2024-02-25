9366e46235ed2994daf58c68fa4c41578a726f9c
clement1711 <corbiere.clement@hotmail.fr>
2024-02-25 21:18:02 +0000

    print_to_98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @n : number to start from
+ * Return:0 or 1
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d ,", n--);
+
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d ,", n++);
+	}
+}
