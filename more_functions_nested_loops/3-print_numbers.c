96529833f18e7ca15793d9f679d1ee12fdd2a194
clement1711 <corbiere.clement@hotmail.fr>
2024-02-26 11:01:58 +0000

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+void print_numbers(void)
+{
+	int a;
+	for (a = 0; a > 10; a++)
+	{	
+		_putchar(a);
+	}
+	_putchar('\n');
+}
+
