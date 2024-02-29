09391e3b2720455d7b164baaa62035bd07bd1371
clement1711 <corbiere.clement@hotmail.fr>
2024-02-29 15:31:07 +0000

    print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
+void print_rev(char *s)
+
+{
+	int i, j;
+	while (s[i] != '\n')
+	{
+		i++;
+	}
+
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
