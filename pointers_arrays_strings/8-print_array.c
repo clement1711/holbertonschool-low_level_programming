39589dfa1b0fa341a6b31fa762c5326a5a5f4abd
clement1711 <clement.corbiere@hotmail.fr>
2024-03-01 14:56:33 +0000

    print_array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,6 +13,12 @@ void print_array(int *a, int n)
 
 	for (x = 0; x < n; x++)
 	{
-		printf("%d", a[x]);
+		if (x != n - 1)
+			printf("%d ,", a[x]);
+
+		else
+			printf("%d", a[x]);
 	}
+
+	printf("\n");
 }
